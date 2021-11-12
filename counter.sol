883b83d60c072f1f2998374dcd18705ac3823367
Otobong Peter <53745898+otobongfp@users.noreply.github.com>
2021-11-12 19:40:52 +0100

    there was no need for public

--- counter.sol
+++ counter.sol
@@ -4,7 +4,7 @@ pragma solidity >=0.7.0 <0.9.0;
 
 contract playGround{
     
-    uint public count = 0;
+    uint count = 0;
   
     function getCount() view public returns(uint){
         return count;
